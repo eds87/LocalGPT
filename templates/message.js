447efab014825dcc55d6f0c7e99e0aa4e738088b
Jose Da Silva <jeduardodsdp@gmail.com>
2024-06-12 16:28:23 +0100

    css improvements

--- templates/message.js
+++ templates/message.js
@@ -6,16 +6,27 @@ class MessageTemplate extends HTMLElement {
     }
   
     async loadTemplate() {
+      const styleResponse = await fetch('styles.css');
+      const styleText = await styleResponse.text();
+      const styleElement = document.createElement('style');
+      styleElement.textContent = styleText;
+    
       const response = await fetch('templates/message.html');
       const templateText = await response.text();
       const templateElement = document.createElement('div');
       templateElement.innerHTML = templateText;
       this.template = templateElement.querySelector('template');
+
+      this.shadowRoot.appendChild(styleElement);
     }
   
     async addMessage(sender, message) {
       await this.templateLoaded;
       const templateContent = this.template.content.cloneNode(true);
+      const messageElement = templateContent.querySelector('.message');
+      messageElement.classList.add(sender);
+      const senderNameElement = templateContent.querySelector('.sender');
+      senderNameElement.classList.add(sender);
       templateContent.querySelector('.sender').textContent = sender;
       templateContent.querySelector('.messageText').textContent = message;
       this.shadowRoot.appendChild(templateContent);
